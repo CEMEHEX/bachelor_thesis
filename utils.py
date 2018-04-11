a714850bbc20b18230f316b64f6769d9b5770089
Semyon <CEMEHEX@yandex.ru>
2018-04-11 14:49:15 +0300

    Added type hints to utils.py

--- utils.py
+++ utils.py
@@ -1,34 +1,42 @@
 import re
 from os import listdir
+from typing import Iterable, Tuple
 
 from os.path import isfile, join
 
 
-def get_name(filename, img_ext, mask_suffix, mask_ext):
+def get_name(filename: str,
+             img_ext: str,
+             mask_suffix: str,
+             mask_ext: str) -> str:
     res = re.sub('\.' + img_ext, '', filename)
     res = re.sub(mask_suffix + '\.' + mask_ext, '', res)
     return res
 
 
-def mask_name(filename, mask_suffix, mask_ext):
+def mask_name(filename: str, mask_suffix: str, mask_ext: str) -> str:
     return '%s%s.%s' % (filename, mask_suffix, mask_ext)
 
 
-def origin_name(filename, img_ext):
+def origin_name(filename: str, img_ext: str) -> str:
     return '%s.%s' % (filename, img_ext)
 
 
-def get_result(dir_path, filename, img_ext, mask_suffix, mask_ext):
+def get_result(dir_path: str,
+               filename: str,
+               img_ext: str,
+               mask_suffix: str,
+               mask_ext: str) -> (str, str):
     origin_path = join(dir_path, origin_name(filename, img_ext))
     mask_path = join(dir_path, mask_name(filename, mask_suffix, mask_ext))
     return origin_path, mask_path
 
 
 def get_data_paths(
-        dir_path,
-        img_ext="jpg",
-        mask_suffix="_mask",
-        mask_ext="png"):
+        dir_path: str,
+        img_ext: str = "jpg",
+        mask_suffix: str = "_mask",
+        mask_ext: str = "png") -> Iterable[Tuple[str, str]]:
     files = [f for f in listdir(dir_path) if isfile(join(dir_path, f))]
     files = map(lambda f: get_name(f, img_ext, mask_suffix, mask_ext), files)
     files = list(set(files))
