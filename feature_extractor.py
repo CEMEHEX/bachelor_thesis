183eb48e523a3127433a5d3a1d3212b2afacc5b0
Semyon <CEMEHEX@yandex.ru>
2018-04-27 17:40:26 +0300

    Implemented feature extraction

--- feature_extractor.py
+++ feature_extractor.py
@@ -4,6 +4,7 @@ from typing import Dict, List, Tuple
 import numpy as np
 
 from colors import ColorT, COLOR_2_TYPE
+from split_generator import generate_img_chunks
 
 
 def chunk_type(mask_chunk: np.ndarray,
@@ -25,24 +26,48 @@ def chunk_descriptor(img_chunk: np.ndarray) -> np.ndarray:
     return pixel_sum // pixel_cnt
 
 
-def extract_features(img: np.ndarray,
-                     mask: np.ndarray,
-                     chunk_size: int = 4) -> List[Tuple[int, np.ndarray]]:
-    pass
+def extract_features(img_path: str,
+                     mask_path: str,
+                     out_path: str,
+                     chunk_size: int = 4,
+                     ) -> None:
+    img_chunks = generate_img_chunks(
+        img_path,
+        size_x=chunk_size,
+        size_y=chunk_size,
+        step_x=chunk_size,
+        step_y=chunk_size
+    )
+
+    mask_chunks = generate_img_chunks(
+        mask_path,
+        size_x=chunk_size,
+        size_y=chunk_size,
+        step_x=chunk_size,
+        step_y=chunk_size
+    )
+    chunks = zip(img_chunks, mask_chunks)
+
+    with open(out_path, 'a') as file:
+        for img_chunk, mask_chunk in chunks:
+            chunk_t = chunk_type(mask_chunk)
+            chunk_desc1, chunk_desc2, chunk_desc3 = chunk_descriptor(img_chunk)
+            file.write(f'{chunk_t},{chunk_desc1},{chunk_desc2},{chunk_desc3}\n')
 
 
 if __name__ == '__main__':
-    img = np.array(
-        [
-            [[10, 10, 0], [20, 20, 20]],
-            [[5, 5, 5], [5, 5, 5]]
-        ]
-    )
-    mask = np.array(
-        [
-            [[224, 224, 224], [224, 224, 224]],
-            [[255, 128, 0], [224, 224, 224]]
-        ]
-    )
-    print(chunk_descriptor(img))
-    print(chunk_type(mask))
+    # img = np.array(
+    #     [
+    #         [[10, 10, 0], [20, 20, 20]],
+    #         [[5, 5, 5], [5, 5, 5]]
+    #     ]
+    # )
+    # mask = np.array(
+    #     [
+    #         [[224, 224, 224], [224, 224, 224]],
+    #         [[255, 128, 0], [224, 224, 224]]
+    #     ]
+    # )
+    # print(chunk_descriptor(img))
+    # print(chunk_type(mask))
+    extract_features('data/water/00.32953.jpg', 'data/water/00.32953_mask.png', 'out/features.csv')
