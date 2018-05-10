2ea86a09d9195b21aa535f1f8312f5f85cb432c9
Semyon <CEMEHEX@yandex.ru>
2018-05-10 15:29:25 +0300

    Changed train_all.sh script

--- train_all.sh
+++ train_all.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
 
-./main.py --batch_size 20 --epochs 120 --train_data data/forest_train --test_data data/forest_test --weights weights/forest.h5 --logs out/forest.csv --train
-./main.py --batch_size 20 --epochs 120 --train_data data/sand_train --test_data data/sand_test --weights weights/sand.h5 --logs out/sand.csv --train
-./main.py --batch_size 20 --epochs 120 --train_data data/clouds_train --test_data data/clouds_test --weights weights/clouds.h5 --logs out/clouds.csv --train
-./main.py --batch_size 20 --epochs 120 --train_data data/ground_train --test_data data/ground_test --weights weights/ground.h5 --logs out/ground.csv --train
-./main.py --batch_size 20 --epochs 120 --train_data data/grass_train --test_data data/grass_test --weights weights/grass.h5 --logs out/grass.csv --train
+python3 ./main.py --batch_size 20 --epochs 120 --class sand --train
+python3 ./main.py --batch_size 20 --epochs 120 --class roads --input_size 64 --train
+python3 ./main.py --batch_size 20 --epochs 120 --class grass --train
+python3 ./main.py --batch_size 20 --epochs 120 --class ground --train
