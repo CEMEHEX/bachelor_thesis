eb60c7de569ee0055558f3367d4a32ffb624ada9
Semyon <CEMEHEX@yandex.ru>
2018-05-10 17:36:45 +0300

    Added other models to metric checker

--- check_method_metrics.py
+++ check_method_metrics.py
@@ -4,7 +4,7 @@ from mask_converters import *
 from metrics import class_jacard_index
 
 if __name__ == '__main__':
-    methods = {'svm', 'unet'}
+    methods = {'unet', 'svm', 'rtrees', 'mlp', 'knearest', 'boost'}
     img_name = '00.32953'
     mask = cv2.imread(f'tmp/{img_name}_mask.png')
     for method in methods:
@@ -13,4 +13,5 @@ if __name__ == '__main__':
         print(f'{method}:')
         for t in TO_BIN_CONVERTERS:
             cur_conv = TO_BIN_CONVERTERS[t]
-            print(f'\t{t}: {class_jacard_index(mask, pred, cur_conv)}')
+            score = class_jacard_index(mask, pred, cur_conv)
+            print(f'\t{t}: {score if score != 1.0 else "not present"}')
