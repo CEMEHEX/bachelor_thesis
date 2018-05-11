86d8abe22dcb72963d24f8f725d9a7316e56b164
Semyon <CEMEHEX@yandex.ru>
2018-05-11 17:08:17 +0300

    train_all updated

--- train_all.sh
+++ train_all.sh
@@ -1,8 +1,14 @@
 #!/usr/bin/env bash
 
-python3 ./main.py --batch_size 20 --epochs 120 --class sand --train
-#python3 ./train_infinite_generator.py
-python3 ./main.py --batch_size 20 --epochs 120 --class roads --input_size 64 --train
-python3 ./main.py --batch_size 20 --epochs 120 --class buildings --input_size 64 --train
 python3 ./main.py --batch_size 20 --epochs 120 --class grass --train
 python3 ./main.py --batch_size 20 --epochs 120 --class ground --train
+
+python3 ./main.py --batch_size 20 --epochs 120 --class water --classic --train
+python3 ./main.py --batch_size 20 --epochs 120 --class forest --classic --train
+python3 ./main.py --batch_size 20 --epochs 120 --class sand --classic --train
+python3 ./main.py --batch_size 20 --epochs 120 --class clouds --classic --train
+python3 ./main.py --batch_size 20 --epochs 120 --class water --classic --train
+python3 ./main.py --batch_size 20 --epochs 120 --class roads --input_size 64 --classic --train
+python3 ./main.py --batch_size 20 --epochs 120 --class buildings --input_size 64 --classic --train
+python3 ./main.py --batch_size 20 --epochs 120 --class grass --classic --train
+python3 ./main.py --batch_size 20 --epochs 120 --class ground --classic --train
