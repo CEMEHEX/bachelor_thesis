9733609603935ac7337b11cb268b287214f49f10
Semyon <CEMEHEX@yandex.ru>
2018-05-14 13:46:41 +0300

    train_all.sh updated

--- train_all.sh
+++ train_all.sh
@@ -1,7 +1,10 @@
 #!/usr/bin/env bash
 
-python3 ./train_infinite_generator.py --classic
-
+python3 ./main.py --batch_size 20 --epochs 120 --class water --train
+python3 ./main.py --batch_size 20 --epochs 120 --class forest --train
+python3 ./main.py --batch_size 20 --epochs 120 --class sand --train
+python3 ./main.py --batch_size 20 --epochs 120 --class clouds --train
+python3 ./main.py --batch_size 20 --epochs 120 --class water --train
 python3 ./main.py --batch_size 20 --epochs 120 --class grass --train
 python3 ./main.py --batch_size 20 --epochs 120 --class ground --train
 
@@ -10,7 +13,5 @@ python3 ./main.py --batch_size 20 --epochs 120 --class forest --classic --train
 python3 ./main.py --batch_size 20 --epochs 120 --class sand --classic --train
 python3 ./main.py --batch_size 20 --epochs 120 --class clouds --classic --train
 python3 ./main.py --batch_size 20 --epochs 120 --class water --classic --train
-python3 ./main.py --batch_size 20 --epochs 120 --class roads --input_size 64 --classic --train
-python3 ./main.py --batch_size 20 --epochs 120 --class buildings --input_size 64 --classic --train
 python3 ./main.py --batch_size 20 --epochs 120 --class grass --classic --train
 python3 ./main.py --batch_size 20 --epochs 120 --class ground --classic --train
